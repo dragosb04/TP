337715a146cbb25e0a097d6a5ddc9cdcbe2e3305
dragosb04 <63968720+dragosb04@users.noreply.github.com>
2024-05-24 23:54:32 +0300

    Create permutari.c

---
+++ Backtracking/permutari.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#define STACK_SIZE 3 // aici modifici dimensiunea permutarilor
+
+int stack[STACK_SIZE + 1];
+
+void swap (int *a, int *b){
+    int aux = *b;
+    *b = *a;
+    *a = aux;
+}
+
+void print_perm (int stack[], int n){
+    for (int i = 1; i <=n; i++)
+        printf("%d ", stack[i]);
+    printf("\n");
+}
+
+void perm_back (int stack[], int k, int n){
+    if (k == n)
+        print_perm(stack, n);
+    
+    for (int i = k; i <= n; i++){
+        swap(&stack[i], &stack[k]);
+        perm_back(stack, k + 1, n);
+        swap(&stack[i], &stack[k]);
+    }
+}
+
+int main (void){
+
+    for (int i = 1; i <= STACK_SIZE; i++)
+        stack[i] = i;
+    
+    int n = STACK_SIZE, k = 1;
+
+    perm_back(stack, k, n);
+
+    return 0;
+}
